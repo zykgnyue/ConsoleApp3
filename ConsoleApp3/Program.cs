816a124b5571180b6279079453a588533f0b8593
zykgnyue <zhangyk@tju.edu.cn>
2018-03-08 18:02:57 +0800

    TEs1

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello  World! Github VersionTESt");
+            Console.WriteLine("Hello  World! Github VersionTEST1");
         }
     }
 }
