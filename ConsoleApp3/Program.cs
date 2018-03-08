794176ebdd4fe24f1f901c057831d0d8b796c410
git <git@192.168.1.109>
2018-03-08 17:07:58 +0800

    Chelc

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApp3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!Git hub Version")
+            Console.WriteLine("Hello World!Git hub Version");
         }
     }
 }
